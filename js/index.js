772215d9bc9a9ee8c9f41a6bc514f11221cf3654
RP-ui <112298889+RP-ui@users.noreply.github.com>
2024-08-29 22:20:16 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("information-test-last-change-time").innerHTML+=infm_wiki.last_change_time;
+document.getElementById("information-test-op").innerHTML+=infm_wiki.creater;
+
+
+document.getElementById("head-img").ondragstart=function(){
+    return false;
+}
