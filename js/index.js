4908c63881ecaee8043f5a8ba48754c9223cb69a
RP-ui <112298889+RP-ui@users.noreply.github.com>
2024-09-01 15:48:30 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
-document.getElementById("page-last-change-time").innerHTML+=infm_wiki.last_change_time;
-document.getElementById("page-creater").innerHTML+=infm_wiki.creater;
+window.onload = function(){
+    document.getElementById("page-last-change-time").innerHTML+=infm_wiki.last_change_time;
+    document.getElementById("page-creater").innerHTML+=infm_wiki.creater;
+    if(infm_wiki.type = "first_of_all"){
+        document.getElementById("page-type").innerHTML+="在一切前面的";
+    }
+    document.getElementById("page-level").innerHTML+=infm_wiki.level;
 
-
-document.getElementById("head-img").ondragstart=function(){
-    return false;
+    document.getElementById("head-img").ondragstart=function(){
+        return false;
+    }
 }
